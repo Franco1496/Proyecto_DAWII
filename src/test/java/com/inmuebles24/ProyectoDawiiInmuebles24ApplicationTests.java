3692483848e970187f572686dbeb63e97728f384
MasterInicial <franco.chavez.g@gmail.com>
2023-05-10 00:58:51 -0500

    primer commit

---
+++ src/test/java/com/inmuebles24/ProyectoDawiiInmuebles24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inmuebles24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDawiiInmuebles24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
