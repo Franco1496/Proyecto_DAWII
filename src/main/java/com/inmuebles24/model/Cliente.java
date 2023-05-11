c6fe72c695ebda76681363c8e053f2c36edfdd69
ebrp-c <ebrp.1290@gmail.com>
2023-05-11 01:24:13 -0500

    añadir columnDefinition para guardado por defecto fecha y hora

--- src/main/java/com/inmuebles24/model/Cliente.java
+++ src/main/java/com/inmuebles24/model/Cliente.java
@@ -1,2 +1,47 @@
-package com.inmuebles24.model;public class Cliente {
+package com.inmuebles24.model;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "clientes")
+@Data
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private long id_cliente;
+
+    @Column(name = "documento", length = 11)
+    private String documento;
+
+    @Column(name = "nom_cliente", length = 100)
+    private String nom_cliente;
+
+    @Column(name = "ape_cliente", length = 100)
+    private String ape_cliente;
+
+    @Column(name = "dir_cliente", length = 100)
+    private String dir_cliente;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_ubigeo", nullable = false)
+    private Ubigeo id_ubigeo;
+
+    @Column(name = "tel_cliente", length = 9)
+    private String tel_cliente;
+
+    @Column(name = "correo_cliente", length = 100)
+    private String correo_cliente;
+
+    @Column(name = "fec_creacion", columnDefinition = "TIMESTAP DEFAULT CURRENT_TIMESTAP")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy/MM/dd hh:mm:ss")
+    private Date fec_creacion;
+
+    @Column(name = "flag_estado")
+    private boolean flag_estado;
 }
--- src/main/java/com/inmuebles24/model/Ubigeo.java
+++ src/main/java/com/inmuebles24/model/Ubigeo.java
@@ -1,2 +1,46 @@
-package com.inmuebles24.model;public class Ubigeo {
+package com.inmuebles24.model;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "ubigeo")
+@Data
+public class Ubigeo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ubigeo")
+    private long id_ubigeo;
+
+    @Column(name = "ubigeo", length = 6)
+    private String ubigeo;
+
+    @Column(name = "cod_departamento", length = 2)
+    private String cod_departamento;
+
+    @Column(name = "departamento", length = 30)
+    private String departamento;
+
+    @Column(name = "cod_provincia", length = 2)
+    private String cod_provincia;
+
+    @Column(name = "provincia", length = 30)
+    private String provincia;
+
+    @Column(name = "cod_distrito", length = 2)
+    private String cod_distrito;
+
+    @Column(name = "distrito", length = 40)
+    private String distrito;
+
+    @Column(name = "fec_creacion", columnDefinition = "TIMESTAP DEFAULT CURRENT_TIMESTAP")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy/MM/dd hh:mm:ss")
+    private Date fec_creacion;
+
+    @Column(name = "flag_estado")
+    private boolean flag_estado;
 }
