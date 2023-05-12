ad09db802cb4d613c734b82343e63c920489d8af
jsantamariag <jaimedarlin.sg@gmail.com>
2023-05-12 17:02:16 -0500

    creación de la interface UnidadMedidaRepository en la capa REPOSITORY

---
+++ src/main/java/com/inmuebles24/repository/UnidadMedidaRepository.java
@@ -0,0 +1,9 @@
+package com.inmuebles24.repository;
+
+import com.inmuebles24.model.UnidadMedida;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnidadMedidaRepository extends JpaRepository<UnidadMedida, Long> {
+}
