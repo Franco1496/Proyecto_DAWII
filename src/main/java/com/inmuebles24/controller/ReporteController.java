1b2d7c8ca01ce8ee9d82ded845e4e54734636b4c
ebrp-c <ebrp.1290@gmail.com>
2023-06-26 05:00:15 -0500

    se agrego agrego anotacion hasRole a ReporteController

--- src/main/java/com/inmuebles24/controller/ReporteController.java
+++ src/main/java/com/inmuebles24/controller/ReporteController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "api/reporte")
+@PreAuthorize("hasRole('ADMIN')")
 public class ReporteController {
 
     @Autowired
